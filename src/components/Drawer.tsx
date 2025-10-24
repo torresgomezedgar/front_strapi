a959e6164ecaeb1f9668accc9bef5df7b46453d7
torresgomezedgar <edgartorresgomez@gmail.com>
2025-10-24 15:45:11 -0500

    update Sidebar

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,14 +1,25 @@
 import Header from "./header/Header"
-import Sidebar from "./header/Sidebar"
+import Sidebar from "./sidebar/Sidebar"
 
 import Content from "./content/Content"
 import Top_header from "./header/Top_header"
+import { useState } from "react"
 
 function Drawer() {
+
+  const [padding, setPadding] = useState(false)
+  
+  const ctlTop = () =>{
+    if (window.scrollY <=0)
+      setPadding(false)
+    else
+      setPadding(true)
+  }
+  console.log(padding)
   return (
     <>
       <div className="drawer">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <input id="my-drawer-2" type="checkbox" onClick={ctlTop} className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
           <Top_header />
@@ -21,7 +32,7 @@ function Drawer() {
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 min-h-full w-80 p-4 pt-20">
+          <ul className={`menu bg-base-200 min-h-full w-80 p-4 ${padding ? "pt-20" : "pt-30" }`}>
             {/* Sidebar content here */}
             <Sidebar />
           </ul>
